e5903098a07e0dd684601dc9d13199e5d2b34108
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-08-13 20:09:43 +0900

    Feat: 같은 위치에서 차 배차해주는 알고리즘 추가

--- Final_CJ_Challenge.py
+++ Final_CJ_Challenge.py
@@ -4,6 +4,52 @@ from tqdm import tqdm
 from Final_K_Means_Clustering import CJ_Cluster
 from FInal_Basic_Information  import Information
 from config import OD_MATRIX_FILE, ORDER_TABLE_FILE, VEHICLE_FILE, TERMINAL_FILE
+from collections import deque
+
+def group_by_cost(orders: list, quantity_by_veh_cbm: dict) -> dict:
+    # weights = sorted(map(lambda x:float(x[4]), orders), reverse=True)
+    weights = sorted(map(lambda x:float(x), orders), reverse=True)
+    sorted_veh_cbm = sorted((key for key, _ in filter(lambda item: item[1] != 0, quantity_by_veh_cbm.items())))
+    groups = dict()
+    dq = deque(weights)
+
+    if weights[0] > sorted_veh_cbm[-1]:
+        raise ValueError(f"{weights[0]}는 남은 차량의 최대 적재량 {sorted_veh_cbm[-1]}를 초과했습니다.")
+
+    while dq:
+        weight = dq.popleft()
+        group = [weight]
+        current_weight = weight
+        
+        for cbm in sorted_veh_cbm:
+            if quantity_by_veh_cbm[cbm] > 0 and weight <= cbm:
+                max_weight = cbm
+                quantity_by_veh_cbm[cbm] -= 1
+                break
+        else:
+            raise ValueError("더이상 배치 가능한 차량이 없습니다.")
+
+        for weight in range(len(dq)):
+            weight = dq.popleft()
+            if current_weight + weight <= max_weight:
+                group.append(weight)
+                current_weight += weight
+            else:
+                dq.append(weight)
+
+        if max_weight not in groups:
+            groups[max_weight] = []
+        groups[max_weight].append(group)
+    
+    # HACK : 예제로 받은 파일에서 27*2 ~= 55 이기에 가능(배수 관계), 그래서 일단.. 그냥 상수 넣었음.. 양심상.. 변수로는 뺐음
+    MIN_CBM = 27
+    MAX_CBM = 55
+    if MIN_CBM*2 <= MAX_CBM and MIN_CBM in groups and len(groups[MIN_CBM]) > 2:
+        if MAX_CBM not in groups:
+            groups[MAX_CBM] = []
+        while len(groups[MIN_CBM]) >= 2:
+            groups[MAX_CBM].append(groups[MIN_CBM].pop()+groups[MIN_CBM].pop())
+    return groups
 
 def possible_combinations_from_orders(batch_orders_in_terminal):
     possible_combinations = [[]]
